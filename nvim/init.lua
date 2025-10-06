0c8b3b1cc882a220388a2d73a2abfb5d7c53e570
Stsiapan Tsikhanau <stsiapan.tsikhanau@gmail.com>
2025-10-06 20:58:15 +0200

    Reset neovim to default actual LazyVim version

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,2 @@
-require("plugins")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- nvim/lua/modules/options.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-function M.setup()
-  vim.opt.number = true
-  vim.opt.relativenumber = true
-end
-
-return M
